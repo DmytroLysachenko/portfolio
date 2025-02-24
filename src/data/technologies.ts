acbbc876da0f36e6cb2d158c11efef396ef64418
DmytroLysachenko <dlysachenko98@gmail.com>
2025-02-24 17:31:51 +0100

    Tech stack update

---
+++ src/data/technologies.ts
@@ -0,0 +1,40 @@
+const technologies = [
+  {
+    category: "Languages",
+    items: [
+      { name: "TypeScript", icon: "/icons/languages/TypeScript.png" },
+      { name: "JavaScript", icon: "/icons/languages/JavaScript.png" },
+      { name: "HTML5", icon: "/icons/languages/HTML5.png" },
+      { name: "CSS3", icon: "/icons/languages/CSS3.png" },
+    ],
+  },
+  {
+    category: "Frameworks/Libraries",
+    items: [
+      { name: "React", icon: "/icons/frameworks/React.png" },
+      { name: "Next.js", icon: "/icons/frameworks/Next.js.png" },
+      { name: "Astro", icon: "/icons/frameworks/Astro.png" },
+      { name: "TailwindCSS", icon: "/icons/frameworks/TailwindCSS.png" },
+    ],
+  },
+  {
+    category: "Databases",
+    items: [
+      { name: "MongoDB", icon: "/icons/databases/MongoDB.png" },
+      { name: "PostgreSQL", icon: "/icons/databases/PostgresSQL.png" },
+      { name: "Redis", icon: "/icons/databases/SQLite.png" },
+    ],
+  },
+  {
+    category: "Instrumentation",
+    items: [
+      { name: "GitHub", icon: "/icons/instruments/GitHub.png" },
+      { name: "Git", icon: "/icons/instruments/Git.png" },
+      { name: "Jira", icon: "/icons/instruments/Jira.png" },
+      { name: "Trello", icon: "/icons/instruments/Trello.png" },
+      { name: "VSC", icon: "/icons/instruments/VSC.png" },
+    ],
+  },
+];
+
+export default technologies;
